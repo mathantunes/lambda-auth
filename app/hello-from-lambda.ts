976325712d083cc3a9dc9dfbf3318f8dbda9328e
Matheus <matheus.antunes.96@gmail.com>
2020-12-15 22:57:42 -0500

    templating

---
+++ app/hello-from-lambda.ts
@@ -0,0 +1,20 @@
+import {
+    APIGatewayProxyEvent,
+    APIGatewayProxyResult
+} from "aws-lambda";
+
+/**
+ * A Lambda function that returns a static string
+ */
+export const helloFromLambdaHandler = async (event: APIGatewayProxyEvent) : Promise<APIGatewayProxyResult> => {
+    // If you change this message, you will need to change hello-from-lambda.test.js
+    const message = 'Hello from Lambda!';
+
+    // All log statements are written to CloudWatch
+    console.info(`${message}`);
+    
+    return {
+        statusCode: 200, 
+        body: message
+    };
+}
